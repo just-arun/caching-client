40c961ee8b346f3e5e66b895caa6222d1893bb30
arun <arunberry47@gmail.com>
2021-03-22 15:53:06 +0530

    linting files

--- src/app/products/products.controller.ts
+++ src/app/products/products.controller.ts
@@ -4,33 +4,29 @@ import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
-    constructor(
-        private productService: ProductsService
-    ) {}
+  constructor(private productService: ProductsService) {}
 
-    @Get("/")
-    async getMany(
-        @Res() res: Response
-    ) {
-        try {
-            const products = await this.productService.getMany();
-            return res.status(200).json({ products })
-        } catch (error) {
-            return res.status(500).json({ error })
-        }
+  @Get('/')
+  async getMany(@Res() res: Response) {
+    try {
+      const products = await this.productService.getMany();
+      return res.status(200).json({ products });
+    } catch (error) {
+      return res.status(500).json({ error });
     }
+  }
 
-    @Get("/:id")
-    async getOne(
-        @Res() res: Response,
-        @Param("id") id: string,
-        @Query("metadata") metadata?: boolean
-    ) {
-       try {
-           const product = await this.productService.getOne(id, metadata);
-           return res.status(200).json({ product })
-       } catch (error) {
-           return res.status(500).json({ error })
-       } 
+  @Get('/:id')
+  async getOne(
+    @Res() res: Response,
+    @Param('id') id: string,
+    @Query('metadata') metadata?: boolean,
+  ) {
+    try {
+      const product = await this.productService.getOne(id, metadata);
+      return res.status(200).json({ product });
+    } catch (error) {
+      return res.status(500).json({ error });
     }
+  }
 }
--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -3,59 +3,63 @@ import { ProductData, ProductDataType } from './products-data';
 
 @Injectable()
 export class ProductsService {
-    public async getMany(): Promise<any> {
-        return ProductData().map(res => (this.leanData(res)));
-    }
+  public async getMany(): Promise<any> {
+    return ProductData().map((res) => this.leanData(res));
+  }
 
-    public async getOne(id: string, metadata?: boolean): Promise<any> {
-        const result = ProductData().filter(res => {
-            if (res._id == id) {
-                return res;
-            }
-        }).map(res => {
-            if (!!metadata) {
-                return {
-                    ...res.seo,
-                    media: res.media,
-                    model: res.model,
-                    specifications: res.specifications,
-                };
-            } else {
-                return res;
-            }
-        })
-        return typeof result[0] != 'undefined' ? result[0] : {};
-    }
-
-    private getStorage(obj){
-        return obj.filter(res => {
-            if (res.code == "storage") return res;
-        }).map(res => res.values)[0]
-        .map(res => res.name)
-    }
-
-    private getColor(colorObj) {
-        return  colorObj
-        .filter(res => {
-            if (res.code == "color") return res;
-        })
-        .map(res => res.values)[0]
-        .map(obj => obj.code)
-    }
-
-    private leanData(res: any) {
-        const getImgUrl = (imgObj) => imgObj.map(img => img.url)
-        return {
-            _id: res._id,
-            brand: res.brand,
-            code: res.code,
-            isActive: res.isActive,
-            images: getImgUrl(res.media.image_gallery),
-            color: this.getColor(res.model.variant_attr_scheme),
-            storage: this.getStorage(res.model.variant_attr_scheme),
-            name: res.name,
-            seo: res.seo,
-            description: res.description,
+  public async getOne(id: string, metadata?: boolean): Promise<any> {
+    const result = ProductData()
+      .filter((res) => {
+        if (res._id == id) {
+          return res;
         }
-    }
+      })
+      .map((res) => {
+        if (!!metadata) {
+          return {
+            ...res.seo,
+            media: res.media,
+            model: res.model,
+            specifications: res.specifications,
+          };
+        } else {
+          return res;
+        }
+      });
+    return typeof result[0] != 'undefined' ? result[0] : {};
+  }
+
+  private getStorage(obj) {
+    return obj
+      .filter((res) => {
+        if (res.code == 'storage') return res;
+      })
+      .map((res) => res.values)[0]
+      .map((res) => res.name);
+  }
+
+  private getColor(colorObj) {
+    return colorObj
+      .filter((res) => {
+        if (res.code == 'color') return res;
+      })
+      .map((res) => res.values)[0]
+      .map((obj) => obj.code);
+  }
+
+  private leanData(res: any) {
+    const getImgUrl = (imgObj) => imgObj.map((img) => img.url);
+    return {
+      _id: res._id,
+      brand: res.brand,
+      code: res.code,
+      isActive: res.isActive,
+      images: getImgUrl(res.media.image_gallery),
+      color: this.getColor(res.model.variant_attr_scheme),
+      storage: this.getStorage(res.model.variant_attr_scheme),
+      name: res.name,
+      seo: res.seo,
+      description: res.description,
+    };
+  }
 }
