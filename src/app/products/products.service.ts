fef523c608780b11f34efd15d8ec932ba963a8d2
arun <arunberry47@gmail.com>
2021-03-22 15:24:01 +0530

    modifying data for metadata

--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -4,26 +4,58 @@ import { ProductData, ProductDataType } from './products-data';
 @Injectable()
 export class ProductsService {
     public async getMany(): Promise<any> {
-        return ProductData().map(res => ({
-            _id: res._id,
-            brand: res.brand,
-            code: res.code,
-            isActive: res.isActive,
-            images: res.media.image_gallery,
-            model: res.model,
-            name: res.name,
-            description: res.description,
-        }));
+        return ProductData().map(res => (this.leanData(res)));
     }
 
     public async getOne(id: string, metadata?: boolean): Promise<any> {
         const result = ProductData().filter(res => {
             if (res._id == id) {
-                if (metadata) {
-                    return res.seo;
-                } return res;
+                return res;
+            }
+        }).map(res => {
+            if (!!metadata) {
+                return {
+                    ...res.seo,
+                    media: res.media,
+                    model: res.model,
+                    specifications: res.specifications,
+                };
+            } else {
+                return res;
             }
         })
-        return result;
+        return typeof result[0] != 'undefined' ? result[0] : {};
+    }
+
+    private getStorage(obj){
+        return obj.filter(res => {
+            if (res.code == "storage") return res;
+        }).map(res => res.values)[0]
+        .map(res => res.name)
+    }
+
+    private getColor(colorObj) {
+        return  colorObj
+        .filter(res => {
+            if (res.code == "color") return res;
+        })
+        .map(res => res.values)[0]
+        .map(obj => obj.code)
+    }
+
+    private leanData(res: any) {
+        const getImgUrl = (imgObj) => imgObj.map(img => img.url)
+        return {
+            _id: res._id,
+            brand: res.brand,
+            code: res.code,
+            isActive: res.isActive,
+            images: getImgUrl(res.media.image_gallery),
+            color: this.getColor(res.model.variant_attr_scheme),
+            storage: this.getStorage(res.model.variant_attr_scheme),
+            name: res.name,
+            seo: res.seo,
+            description: res.description,
+        }
     }
 }
